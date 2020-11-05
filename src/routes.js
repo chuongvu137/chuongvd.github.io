3f7becae93a7f47a25a1c838ed80a5088e310cd8
ChuongVD <chuong@spikehub.com>
2020-11-05 18:31:50 +0700

    update

--- src/routes.js
+++ src/routes.js
@@ -3,16 +3,16 @@ import BlogListContainer from './containers/BlogListContainer';
 import BlogDetailContainer from './containers/BlogDetailContainer';
 
 const routes = [
-    {
-        path: '/',
-        exact: true,
-        main: ({match}) => <BlogListContainer match={match}/>
-    },
-    {
-        path: '/detail/:id',
-        exact: false,
-        main: ({match}) => <BlogDetailContainer match={match}/>
-    }
+  {
+    path: '/',
+    exact: true,
+    main: () => <BlogListContainer />,
+  },
+  {
+    path: '/detail/:id',
+    exact: false,
+    main: ({ match }) => <BlogDetailContainer match={match} />,
+  },
 ];
 
 export default routes;
